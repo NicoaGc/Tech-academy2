196fb20ca6e2664cc68a29bc089f870bb5e81cdf
joseph-0505 <sjosevictor31@gmail.com>
2025-04-29 22:27:20 -0300

    atividades tech forge 29/04/25

--- exercicio1.js
+++ exercicio1.js
@@ -1,3 +1,10 @@
 let numeros = [1,2,9,7,6,4,10];
 
 console.log(numeros[3]);
+
+const arraynumeros = [1, 7, 8, 2, 4, 6, 9];
+let concatenacao="";
+
+for (let i=0; i < 7 i=i+1 ){
+    console.log(i + " -> " + arraynumeros[i]);
+}
