89969efa786dded383ec9ebb45602ef6d1c17f8a
joseph-0505 <sjosevictor31@gmail.com>
2025-04-29 22:35:33 -0300

    asasa

--- atividade2.js
+++ atividade2.js
@@ -1,2 +1,13 @@
-let numeros = [1, 7, 8, 2, 4, 6, 9];
-let
+const prompt = require('prompt-sync')();
+let numeros = [1,2,3,4,5,6,7,8,9,10];
+let media = [];
+let soma = 0,qtd;
+
+qtd = prompt("Digite a quantidade de números que deseja calcular a média: ");
+
+for (let i = 0; i <qtd; i++) {
+    numeros[i] = number(prompt("Digite o número: "+i));
+    soma = soma + numeros[i];
+}
+media=soma /7;
+console.log(media);
